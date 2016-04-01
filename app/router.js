502813285ce8bede70cbf654c7f20d0772f1f749
Gracie Lundell <gracielundell@gmail.com>
2016-04-01 09:14:56 -0700

    fix index route to squash routing error

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('post');
+  this.route('question');
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });
