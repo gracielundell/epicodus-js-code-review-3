661c67022a19c4a92bd4712e117c08da962f81a4
Gracie Lundell <gracielundell@gmail.com>
2016-04-08 11:31:07 -0700

    print comment in favorite-tile (not yet on favorite route

--- app/components/favorite-tile.js
+++ app/components/favorite-tile.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // favoriteAnswers: Ember.inject.service(),
-  // actions: {
-  //   addToFavorites(item) {
-  //     this.get('favoriteAnswers').add(item);
-  //   }
-  // }
+  favoriteAnswers: Ember.inject.service(),
+  actions: {
+    addToFavorites(item) {
+      this.get('favoriteAnswers').add(item);
+    }
+  }
 });
