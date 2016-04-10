0476d8871ab294f9fcc9e7c2a4b899028349fa40
Gracie Lundell <gracielundell@gmail.com>
2016-04-09 22:16:22 -0700

    add class to change favorite button when clicked to indicate a comment had been favorited

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bestAnswer: false,
   favoriteAnswers: Ember.inject.service(),
   actions: {
     addToFavorites(item) {
+      this.set('bestAnswer', true);
       this.get('favoriteAnswers').add(item);
       this.sendAction('addToFavorites', item);
       // send item(answer) into favorites pg
