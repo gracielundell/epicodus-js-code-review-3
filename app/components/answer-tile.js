62167160eda2d37c9aa19c6a38bbf642356763fe
Gracie Lundell <gracielundell@gmail.com>
2016-04-13 08:13:01 -0700

    add notes for computed properties, helper, and service

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   bestAnswer: false,
+  // add service to send favorited answers into array, display on favorite route
   favoriteAnswers: Ember.inject.service(),
   actions: {
     addToFavorites(item) {
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // computed property to combine user and question
   fullQuestioner: Ember.computed('question.author', 'question.title', function() {
     return this.get('question.author') + ' asks: ' + '"' + this.get('question.title') + '"';
   }),
